670854b94d4f078b043ca861b940863b2bbac032
Vjekoslav Ratkajec <vjeko@labtiva.com>
2017-04-21 15:56:51 +0200

    Initial commit.

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+(() => {
+  // The initialize function must be run each time a new page is loaded
+  Office.initialize = (reason) => {
+    $(document).ready(() => {
+      $('#run').click(run);
+    });
+  };
+
+  async function run() {
+    
+    await Word.run(async (context) => {
+      /**
+       * Insert your Word code here
+       */
+      await context.sync();
+
+      var foo = Office.context.document.settings.get('hello');
+      if (!foo) {
+        Office.context.document.settings.set('hello', 'world');
+        Office.context.document.settings.saveAsync(function (asyncResult) {
+          $('#content').append('Settings saved with status: ' + asyncResult.status);
+        });
+      }
+      else {
+        $('#content').append('Value found: ' + foo);
+      }
+
+    });
+    
+  }
+})();
